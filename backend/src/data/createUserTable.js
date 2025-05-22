f966b19866c896a845f41da0df8487ac6a96a2a6
guimartinsalmeida <guimartinsalmeida@gmail.com>
2025-05-22 16:04:43 -0300

    adding Neon db connection, and adding .env.example

--- backend/src/data/createUserTable.js
+++ backend/src/data/createUserTable.js
@@ -4,8 +4,9 @@ export const createUserTable = async () => {
   console.log('createUserTable() foi chamada') // <--- aqui
   const query = `CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
   )`
 
