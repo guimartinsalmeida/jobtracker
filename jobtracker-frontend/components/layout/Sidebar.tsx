73694bb5d0fdd95bb7bba9ceae8ecbf74fb39df2
guimartinsalmeida <guimartinsalmeida@gmail.com>
2025-05-29 16:52:28 -0300

    adding favicon and title branded and icon at lateral bar menu

--- jobtracker-frontend/components/layout/Sidebar.tsx
+++ jobtracker-frontend/components/layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { FiHome, FiBriefcase, FiCalendar, FiBarChart2, FiFileText, FiUser } from 'react-icons/fi';
 import { SidebarItem } from '@/components/layout/SidebarItem';
 import { usePathname } from 'next/navigation';
@@ -7,7 +8,10 @@ export function Sidebar() {
 
   return (
     <aside className="bg-[#181F2A] w-56 min-h-screen flex flex-col py-6 px-4">
-      <h2 className="text-xl font-bold text-blue-400 mb-8">JobTrackr</h2>
+      <div className='flex justify-center items-center mb-8 gap-3'>
+        <Image src="/favicon.png" alt="JobTracker" width={32} height={32} />
+        <h2 className="text-xl font-bold text-blue-400">JobTracker</h2>
+      </div>
       <nav className="flex flex-col gap-2">
         <SidebarItem icon={<FiHome />} label="Dashboard" href="/" active={pathname === '/'} />
         <SidebarItem icon={<FiBriefcase />} label="Jobs" href="/home" active={pathname === '/home'} />
